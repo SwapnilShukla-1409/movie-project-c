602b7e94edccfceeeea04942fa17754b9b00ad7f
unknown <swapnilshukla1409@gmail.com>
2025-10-31 01:59:04 +0530

    Migrated project from MySQL to PostgreSQL

---
+++ backend/prisma/migrations/20251030202450_init_postgres/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "director" TEXT NOT NULL,
+    "budget" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "duration" TEXT NOT NULL,
+    "year" TEXT NOT NULL,
+    "posterUrl" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
